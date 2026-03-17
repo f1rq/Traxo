ec1c42ce5282aca527e70057abba4ee9db8ed99b
f1rq <f.czudaj@tutanota.com>
2026-03-17 20:53:17 +0100

    Initial Commit

---
+++ Traxo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Traxo
+//
+//  Created by Fabio Czudaj on 17/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Traxo/TraxoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraxoApp.swift
+//  Traxo
+//
+//  Created by Fabio Czudaj on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TraxoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
