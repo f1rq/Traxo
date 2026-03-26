711e530c5a2f080badd7ebcb9c52faa9063d1693
f1rq <f.czudaj@tutanota.com>
2026-03-26 15:32:57 +0100

    feat: add title and format data in Ride model

--- Traxo/Models/Ride.swift
+++ Traxo/Models/Ride.swift
@@ -10,13 +10,39 @@ import Foundation
 
 @Model
 class Ride {
+    var title: String
     var distance: Double
     var duration: Int
     var date: Date
     
     init(distance: Double, duration: Int, date: Date) {
+        self.title = "Untitled ride"
         self.distance = distance
         self.duration = duration
         self.date = date
+    }
+}
+
+extension Ride {
+    var formattedDistance: String {
+        distance.formatted(.number.precision(.fractionLength(1))) + " km"
+    }
+    
+    var formattedDuration: String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .abbreviated
+        return formatter.string(from: TimeInterval(duration)) ?? ""
+    }
+    
+    var formattedDate: String {
+        let calendar = Calendar.current
+        let rideYear = calendar.component(.year, from: date)
+        let currentYear = calendar.component(.year, from: Date())
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = (rideYear == currentYear) ? "dd MMMM, HH:mm" : "dd MMMM yyyy, HH:mm"
+        
+        return formatter.string(from: date)
     }
 }
